b704b5510c0004797d10f5f5fd1c3a56a91e8421
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-04-30 12:34:58 +0200

    adding error response to  SignInLandingComponent

--- src/components/Account/SignInLandingComponent.js
+++ src/components/Account/SignInLandingComponent.js
@@ -5,23 +5,37 @@ import { getAuthCookie, isStateValid } from "./authHelper";
 const SignInLandingComponent = () => {
   const { requestAccessToken } = useAuth();
   const [stateFromCookies, setStateFromCookies] = useState(getAuthCookie());
-
-  // HACK: If error response received: display an error message to the user
+  const [error, setError] = useState(null);
+  const [errorDescription, setErrorDescription] = useState(null);
 
   useEffect(() => {
-    // Verify the state parameter and extract the code from the URL
+    // Verify the state parameter and extract the code and error parameters from the URL
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
     const state = urlParams.get("state");
+    const error = urlParams.get("error");
 
-    // Implement the logic to verify the state parameter
-    // If the state is valid, call the requestAccessToken function with the code
-    if (isStateValid(state, stateFromCookies)) {
+    if (error) {
+      setError(error);
+    } else if (isStateValid(state, stateFromCookies)) {
+      // If the state is valid, call the requestAccessToken function with the code
       requestAccessToken(code);
     }
   }, [requestAccessToken]);
 
-  const content = <h4>Processing Auth...</h4>;
+  let content;
+  if (error) {
+    content = (
+      <>
+        <h4>An error occurred during authentication:</h4>
+        <p>
+          <strong>Error:</strong> {error}
+        </p>
+      </>
+    );
+  } else {
+    content = <h4>Processing Auth...</h4>;
+  }
 
   return <div>{content}</div>;
 };
