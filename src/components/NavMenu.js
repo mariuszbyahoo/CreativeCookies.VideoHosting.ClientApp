779971360fcdb3ac6e5c77df708f35f891fa5856
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-05-02 11:54:07 +0200

    decrypting accessToken and modifying NavMenu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Collapse,
   NavbarBrand,
@@ -12,72 +12,63 @@ import "./NavMenu.css";
 import LoginComponent from "./Account/Login";
 import RegisterComponent from "./Account/Register";
 import LogoutComponent from "./Account/Logout";
+import { useAuth } from "./Account/AuthContext";
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  const { isAuthenticated, userEmail } = useAuth();
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor(props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar
-          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
-          container
-          light
+  return (
+    <header>
+      <Navbar
+        className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+        container
+        light
+      >
+        <NavbarBrand tag={Link} to="/">
+          MyHub
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse
+          className="d-sm-inline-flex flex-sm-row-reverse"
+          isOpen={!collapsed}
+          navbar
         >
-          <NavbarBrand tag={Link} to="/">
-            MyHub
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse
-            className="d-sm-inline-flex flex-sm-row-reverse"
-            isOpen={!this.state.collapsed}
-            navbar
-          >
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/films-list">
-                  Films list
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/films-upload">
-                  Film upload
-                </NavLink>
-              </NavItem>
-              {/* HACK: warunkowo wyświetl komponent linków logowania i rejestrowania, albo adres email użytkownika - ten link będzie prowadził do strony z zarządzaniem konta. */}
-              <NavItem>
-                <LoginComponent className="text-dark  nav-link" />
-              </NavItem>
-              <NavItem>
-                <RegisterComponent className="text-dark  nav-link" />
-              </NavItem>
-              <NavItem>
-                <LogoutComponent className="text-dark  nav-link" />
-              </NavItem>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
-}
+          <ul className="navbar-nav flex-grow">
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/films-list">
+                Films list
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/films-upload">
+                Film upload
+              </NavLink>
+            </NavItem>
+            {/* HACK: warunkowo wyświetl komponent linków logowania i rejestrowania, albo adres email użytkownika - ten link będzie prowadził do strony z zarządzaniem konta. */}
+            <NavItem>
+              <LoginComponent className="text-dark  nav-link" />
+            </NavItem>
+            <NavItem>
+              <RegisterComponent className="text-dark  nav-link" />
+            </NavItem>
+            <NavItem>
+              <LogoutComponent className="text-dark  nav-link" />
+            </NavItem>
+          </ul>
+        </Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default NavMenu;
