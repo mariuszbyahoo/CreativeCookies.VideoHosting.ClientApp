ba3543d516694cf5b49ff68154909c51b0e8f953
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-05-02 13:22:03 +0200

    generating PKCE onClick instead of onRender

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,33 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "./AuthContext";
 import { generateRandomString, setAuthCookie } from "./authHelper";
 
 const LoginComponent = (props) => {
   const { generatePkceData } = useAuth();
-  const { codeVerifier, codeChallenge } = generatePkceData();
   const clientId = process.env.REACT_APP_CLIENT_ID;
   const redirectUri = encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
   const responseType = "code";
-  const scope = encodeURIComponent("test");
-  const state = generateRandomString(32);
-  const encodedState = encodeURIComponent(state);
-
-  const encodedCodeChallenge = encodeURIComponent(codeChallenge);
   const codeChallengeMethod = "S256";
+  const scope = "test";
 
   const handleLoginClick = () => {
+    const { codeVerifier, codeChallenge } = generatePkceData();
+    const state = generateRandomString(32);
+    const encodedState = encodeURIComponent(state);
+    const encodedCodeChallenge = encodeURIComponent(codeChallenge);
+
     setAuthCookie(process.env.REACT_APP_STATE_COOKIE_NAME, state);
-    // Store the codeVerifier in sessionStorage or another appropriate storage
     setAuthCookie(
       process.env.REACT_APP_CODE_VERIFIER_COOKIE_NAME,
       codeVerifier
     );
+    const loginUrl = `https://${process.env.REACT_APP_API_ADDRESS}/api/auth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&state=${encodedState}&code_challenge=${encodedCodeChallenge}&code_challenge_method=${codeChallengeMethod}`;
+
+    window.location.href = loginUrl;
   };
 
-  const loginUrl = `https://${process.env.REACT_APP_API_ADDRESS}/api/auth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&state=${encodedState}&code_challenge=${encodedCodeChallenge}&code_challenge_method=${codeChallengeMethod}`;
-
   return (
-    <a href={loginUrl} className={props.className} onClick={handleLoginClick}>
+    <a className={props.className} onClick={handleLoginClick}>
       Login
     </a>
   );
