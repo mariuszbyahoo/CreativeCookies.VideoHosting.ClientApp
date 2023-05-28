149814d4934f93a3d9485bffe57dc2fd4eb1d7bc
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-05-28 15:00:31 +0200

    styling ProgressDialog as desired

--- src/components/ProgressDialog.js
+++ src/components/ProgressDialog.js
@@ -10,7 +10,26 @@ const ProgressDialog = ({ max, progress, open, title, textToDisplay }) => (
     <Dialog open={open} disableEscapeKeyDown>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h4>{`${((progress / max) * 100).toFixed(0)}%`}</h4>
+        </div>
         <LinearProgress variant="determinate" value={(progress / max) * 100} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          Please do not close nor refresh this page till the upload will be
+          finished.
+        </div>
       </DialogContent>
     </Dialog>
   </>
