f89bc217b2432cfb6039546916c720c638486f49
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-05-29 11:48:48 +0200

    getting placing relevant editor controls

--- src/components/FilmEditor.js
+++ src/components/FilmEditor.js
@@ -1,7 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import styles from "./FilmUpload.module.css";
+import { quillFormats, quillModules } from "./Helpers/quillHelper";
+import { Button, Input } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import ReactQuill from "react-quill";
+import ConfirmationDialog from "./ConfirmationDialog";
 
 const FilmEditor = (props) => {
+  const [videoTitle, setVideoTitle] = useState("");
+  const [description, setVideoDescription] = useState("");
+  const [videoEditFinished, setVideoEditFinished] = useState(false);
+
   const params = useParams();
   const navigate = useNavigate();
 
@@ -14,15 +24,63 @@ const FilmEditor = (props) => {
     const response = await fetch(
       `https://${process.env.REACT_APP_API_ADDRESS}/api/Blobs/getMetadata?id=${params.id}`
     );
-    const responseData = response.json();
-    console.log(`retrieved metadata for video: ${params.id}`);
-    console.log(JSON.stringify(responseData));
+    const responseData = await response.json();
+    setVideoTitle(responseData.name || "");
+    setVideoDescription(responseData.description || "");
+  };
+
+  const videoTitleChangeHandler = (e) => {
+    setVideoTitle(e.target.value);
+  };
+
+  const descriptionChangeHandler = (e) => {
+    setVideoDescription(e);
+  };
+
+  const editVideoHandler = (e) => {
+    console.log("edited!");
   };
 
   return (
     <>
-      <h1>Editor</h1>
-      <p>Of a video with an Id of: {params.id}</p>
+      <div className={styles.container}>
+        <h3 style={{ marginBottom: "5%" }}>Edit video metadata</h3>
+        <div className={`row ${styles["row-margin"]}`}>
+          <label htmlFor="title-input">Title</label>
+          <Input
+            id="title-input"
+            type="text"
+            value={videoTitle}
+            onChange={videoTitleChangeHandler}
+          />
+        </div>
+        <div className={`row ${styles["row-margin"]}`}>
+          <ReactQuill
+            theme="snow"
+            value={description}
+            onChange={descriptionChangeHandler}
+            modules={quillModules}
+            formats={quillFormats}
+          />
+        </div>
+        <Button
+          variant="contained"
+          endIcon={<EditIcon />}
+          onClick={editVideoHandler}
+          style={{ marginTop: "5%" }}
+        >
+          Edit
+        </Button>
+      </div>
+      <ConfirmationDialog
+        title="Success"
+        message="Video has been edited succesfully"
+        open={videoEditFinished}
+        hasCancelOption={false}
+        onConfirm={() => {
+          setVideoEditFinished((prevState) => !prevState);
+        }}
+      ></ConfirmationDialog>
     </>
   );
 };
