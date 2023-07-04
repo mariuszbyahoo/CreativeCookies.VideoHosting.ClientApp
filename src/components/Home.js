ce39ec688c08b156c8fba848fba27c185f8ee46c
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-07-04 20:16:22 +0200

    adding link to films list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Card } from "reactstrap";
 
 export class Home extends Component {
@@ -6,6 +7,9 @@ export class Home extends Component {
     return (
       <Card>
         <h1>Here's the homepage, a place to insert various stuff inside</h1>
+        <h4 style={{ textAlign: "center" }}>
+          Click <Link to="/films-list">Here</Link> to explore all films
+        </h4>
       </Card>
     );
   }
