766d7e25ca3fef1c440b4990417f9778138db34b
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-08-13 19:35:56 +0200

    adding nice Stripe indicator

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Collapse,
   NavbarBrand,
@@ -13,14 +13,109 @@ import LoginComponent from "./Account/Login";
 import RegisterComponent from "./Account/Register";
 import LogoutLinkComponent from "./Account/LogoutLink";
 import { useAuth } from "./Account/AuthContext";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, IconButton, Menu, MenuItem } from "@mui/material";
+import {
+  CheckCircleOutlineRounded,
+  ErrorOutlineOutlined,
+  HighlightOffRounded,
+  HourglassBottomRounded,
+  KeyboardArrowDownRounded,
+  SettingsRounded,
+} from "@mui/icons-material";
 
 const NavMenu = () => {
-  const { isAuthenticated, userEmail, userRole, isUserMenuLoading } = useAuth();
+  const {
+    isAuthenticated,
+    userEmail,
+    userRole,
+    isUserMenuLoading,
+    stripeAccountStatus,
+    stripeAccountVerificationPending,
+  } = useAuth();
   const [collapsed, setCollapsed] = useState(true);
+  const [paymentNavContent, setPaymentNavContent] = useState(
+    <>
+      <span style={{ fontWeight: 300, fontSize: 9, marginRight: "1%" }} />
+      <NavItem className="text-purple">
+        Stripe <CircularProgress size={10} />
+      </NavItem>
+    </>
+  );
+
+  useEffect(() => {
+    switch (stripeAccountStatus.data) {
+      case 1:
+        setPaymentNavContent(
+          <>
+            <span style={{ fontWeight: 300, fontSize: 9, marginRight: "1%" }}>
+              partners with:
+            </span>
+            <NavItem className="no-wrap">
+              <a
+                className="nav-link text-orange"
+                href={`https://${process.env.REACT_APP_API_ADDRESS}/Identity/Account/StripeOnboarding`}
+              >
+                Stripe <ErrorOutlineOutlined />
+              </a>
+            </NavItem>
+          </>
+        );
+        break;
+      case 2:
+        setPaymentNavContent(
+          <>
+            <span style={{ fontWeight: 300, fontSize: 9, marginRight: "1%" }}>
+              partners with:
+            </span>
+            <NavItem className="no-wrap text-green">
+              Stripe <CheckCircleOutlineRounded />
+              <IconButton>
+                <SettingsRounded style={{ color: "black" }} />
+              </IconButton>
+            </NavItem>
+          </>
+        );
+        break;
+      case 3:
+        setPaymentNavContent(
+          <>
+            <span style={{ fontWeight: 300, fontSize: 9, marginRight: "1%" }}>
+              partners with:
+            </span>{" "}
+            <NavItem className="no-wrap text-purple">
+              Stripe <HourglassBottomRounded />
+            </NavItem>
+          </>
+        );
+        break;
+      default:
+        setPaymentNavContent(
+          <>
+            <span style={{ fontWeight: 300, fontSize: 9, marginRight: "1%" }}>
+              partners with:
+            </span>{" "}
+            <NavItem className="text-red">
+              <a
+                className="nav-link"
+                href={`https://${process.env.REACT_APP_API_ADDRESS}/Identity/Account/StripeOnboarding`}
+              >
+                Stripe <HighlightOffRounded />
+              </a>
+            </NavItem>
+          </>
+        );
+        break;
+    }
+  }, [stripeAccountStatus, stripeAccountVerificationPending]);
 
   const toggleNavbar = () => {
     setCollapsed(!collapsed);
+  };
+
+  const returnPaymentNav = () => {
+    if (userRole === "ADMIN" || userRole === "admin") {
+      return paymentNavContent;
+    }
   };
 
   const accountNav = () => {
@@ -83,6 +178,7 @@ const NavMenu = () => {
         <NavbarBrand tag={Link} to="/">
           MyHub
         </NavbarBrand>
+        {returnPaymentNav()}
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
         <Collapse
           className="d-sm-inline-flex flex-sm-row-reverse"
