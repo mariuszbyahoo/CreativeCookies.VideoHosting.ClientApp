b81af23d2963c20762245533e7c99bef8eb699f0
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-09-09 09:54:06 +0200

    changing the form of upsertion into dialog

--- src/components/Stripe/Dashboard/ProductUpsertForm.js
+++ src/components/Stripe/Dashboard/ProductUpsertForm.js
@@ -1,9 +1,23 @@
 import { AddCircleOutline } from "@mui/icons-material";
-import { IconButton, TextField } from "@mui/material";
+import {
+  CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Input,
+  TextField,
+} from "@mui/material";
 import { useForm } from "react-hook-form";
 import styles from "./ProductUpsertForm.module.css";
+import { useState } from "react";
 
-const ProductUpsertForm = (props) => {
+const ProductUpsertForm = ({
+  isDialogOpened,
+  setIsDialogOpened,
+  isProcessingProduct,
+  setStripeProduct,
+}) => {
   const {
     register,
     handleSubmit,
@@ -15,6 +29,8 @@ const ProductUpsertForm = (props) => {
     setValue,
     clearErrors,
   } = useForm();
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDesctiption] = useState("");
 
   const fetchWithCredentials = (url, options) => {
     return fetch(url, { ...options, credentials: "include" });
@@ -22,53 +38,101 @@ const ProductUpsertForm = (props) => {
 
   const handleSaveProduct = async () => {
     try {
+      const requestBody = {
+        name: productName,
+        description: productDescription,
+      };
       var res = await fetchWithCredentials(
-        `${process.env.REACT_APP_API_ADDRESS}/StripeProducts/UsertSubscriptionPlan`,
+        `https://${process.env.REACT_APP_API_ADDRESS}/StripeProducts/UpsertSubscriptionPlan`,
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           credentials: "include",
-          body: JSON.stringify(props.stripeProduct),
+          body: JSON.stringify(requestBody),
         }
       );
-      props.setIsEditingProduct(false);
+      if (res.ok) {
+        const productJson = await res.json();
+        setStripeProduct(productJson);
+      } else {
+        // HACK: Manage other responses.
+      }
+      setIsDialogOpened(false);
+      setProductDesctiption("");
+      setProductName("");
     } catch (error) {
       console.error("Error updating product:", error);
+      setIsDialogOpened(false);
     }
   };
 
+  const handleClose = () => {
+    setIsDialogOpened(false);
+  };
+
+  const productNameChangeHandler = (e) => setProductName(e.target.value);
+  const productDescriptionChangeHandler = (e) =>
+    setProductDesctiption(e.target.value);
+
+  const headerText = isProcessingProduct
+    ? "Processing product..."
+    : "Create subscription plan:";
+
   return (
     <>
-      <h3>Create subscription plan:</h3>
-      <form onSubmit={handleSubmit(handleSaveProduct)}>
-        <div className={styles.container}>
-          <TextField
-            label="Name"
-            variant="standard"
-            color="primary"
-            style={{ width: "40%", margin: "2%" }}
-          />
-          <br />
-          <TextField
-            label="Description"
-            variant="standard"
-            color="primary"
-            multiline
-            style={{ width: "40%", margin: "2%" }}
-            minRows={3}
-          />
-          <br />
-          <IconButton
-            color="primary"
-            aria-label="add new product"
-            type="submit"
-          >
-            <AddCircleOutline style={{ fontSize: "48px" }} />
-          </IconButton>
-        </div>
-      </form>
+      <Dialog open={isDialogOpened} onClose={handleClose}>
+        <DialogTitle>{headerText}</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit(handleSaveProduct)}>
+            <div className={styles.container}>
+              <label for="title-input">Product title</label>
+              <Input
+                {...register("productName", {
+                  required: "Product name is required",
+                  minLength: {
+                    value: 5,
+                    message: "Product name must be at least 5 characters",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "Product name cannot exceed 50 characters",
+                  },
+                })}
+                id="title-input"
+                type="text"
+                value={productName}
+                onChange={productNameChangeHandler}
+                color="primary"
+                style={{ width: "25vmax", margin: "2%" }}
+              />
+              {errors.productName && (
+                <p style={{ color: "#b71c1c" }}>{errors.productName.message}</p>
+              )}
+              <label for="text-description">Product description</label>
+              <TextField
+                id="text-description"
+                variant="standard"
+                color="primary"
+                multiline
+                style={{ width: "25vmax", margin: "2%" }}
+                minRows={3}
+                value={productDescription}
+                onChange={productDescriptionChangeHandler}
+              />
+              <br />
+              <IconButton
+                color="primary"
+                aria-label="add new product"
+                type="submit"
+              >
+                <AddCircleOutline style={{ fontSize: "48px" }} />
+              </IconButton>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
--- src/components/Stripe/Dashboard/StripeProductsDashboard.js
+++ src/components/Stripe/Dashboard/StripeProductsDashboard.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {
+  Box,
+  CircularProgress,
+  IconButton,
+  Modal,
   Table,
   TableBody,
   TableCell,
@@ -9,125 +13,53 @@ import {
 } from "@mui/material";
 import styles from "./StripeProductsDashboard.module.css";
 import ProductUpsertForm from "./ProductUpsertForm";
-import { Button } from "reactstrap";
+import { AddCircleOutline } from "@mui/icons-material";
 
 const StripeProductsDashboardComponent = () => {
   const [stripeProduct, setStripeProduct] = useState(null);
   const [stripePrices, setStripePrices] = useState([]);
-  const [isEditingProduct, setIsEditingProduct] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDialogOpened, setIsDialogOpened] = useState(false);
 
   const fetchWithCredentials = (url, options) => {
     return fetch(url, { ...options, credentials: "include" });
   };
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const lookupResponse = await fetchWithCredentials(
-          `https://${process.env.REACT_APP_API_ADDRESS}/StripeProducts/HasAnyProduct`
+        const productResponse = await fetchWithCredentials(
+          `https://${process.env.REACT_APP_API_ADDRESS}/StripeProducts/FetchSubscriptionPlan`
         );
-        const isThereAnyProduct = await lookupResponse.json();
-        if (isThereAnyProduct) {
-          const productResponse = await fetchWithCredentials(
-            `https://${process.env.REACT_APP_API_ADDRESS}/StripeProducts/FetchSubscriptionPlan`
-          );
-          const productData = await productResponse.json();
-          // HACK: Fix this to be corresponding to the latest API changes
-          const pricesResponse = await fetchWithCredentials(
-            `https://${process.env.REACT_APP_API_ADDRESS}/StripePrices/GetAll`
-          );
-          const pricesData = await pricesResponse.json();
-
-          if (productData.length > 0) {
-            setStripeProduct(productData[0]);
-            setStripePrices(pricesData);
-          }
+        const productData =
+          productResponse.status === 200
+            ? await productResponse.json()
+            : undefined;
+        if (productData) {
+          setStripeProduct(productData);
+        } else {
         }
+        setIsLoading(false);
+        // HACK: Fix this to be corresponding to the latest API changes
+        // const pricesResponse = await fetchWithCredentials(
+        //   `https://${process.env.REACT_APP_API_ADDRESS}/StripePrices/GetAll`
+        // );
+        // const pricesData = await pricesResponse.json();
+
+        //}
       } catch (error) {
         console.error("Error fetching Stripe data:", error);
+        setIsLoading(false);
       }
     };
+    setIsLoading(true);
     fetchData();
-  }, []);
-
-  const handleEditProduct = () => {
-    setIsEditingProduct(true);
-  };
-
-  const handleSaveProduct = async () => {
-    try {
-      await fetchWithCredentials(
-        `${process.env.REACT_APP_API_ADDRESS}/StripeProducts/UpdateSubscriptionPlan`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(stripeProduct),
-        }
-      );
-      setIsEditingProduct(false);
-    } catch (error) {
-      console.error("Error updating product:", error);
-    }
-  };
+  }, [isDialogOpened]);
 
   return (
     <div className={styles.container}>
       {stripeProduct ? (
         <div>
-          <h2>Product</h2>
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>{stripeProduct.id}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>
-                  {isEditingProduct ? (
-                    <TextField
-                      value={stripeProduct.name}
-                      onChange={(e) =>
-                        setStripeProduct({
-                          ...stripeProduct,
-                          name: e.target.value,
-                        })
-                      }
-                    />
-                  ) : (
-                    stripeProduct.name
-                  )}
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Description</TableCell>
-                <TableCell>{stripeProduct.description}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleEditProduct}
-                  >
-                    Edit
-                  </Button>
-                  {isEditingProduct && (
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={handleSaveProduct}
-                    >
-                      Save
-                    </Button>
-                  )}
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-          <h2>Prices</h2>
+          <h4>Manage prices for: {stripeProduct.name}</h4>
           <Table>
             <TableHead>
               <TableRow>
@@ -148,11 +80,37 @@ const StripeProductsDashboardComponent = () => {
           </Table>
         </div>
       ) : (
-        <ProductUpsertForm
-          stripeProduct={stripeProduct}
-          setIsEditingProduct={setIsEditingProduct}
-        />
+        <>
+          {isLoading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <>
+              <h3>Subscription plan</h3>
+              <p>
+                Create your first subscription plan to start monetizing your
+                videos
+              </p>
+              {!isDialogOpened && (
+                <IconButton
+                  color="primary"
+                  aria-label="add new product"
+                  onClick={() => setIsDialogOpened(true)}
+                >
+                  <AddCircleOutline style={{ fontSize: "48px" }} />
+                </IconButton>
+              )}
+            </>
+          )}
+        </>
       )}
+      <ProductUpsertForm
+        stripeProduct={stripeProduct}
+        setStripeProduct={(data) => {
+          setStripeProduct(data);
+        }}
+        isDialogOpened={isDialogOpened}
+        setIsDialogOpened={setIsDialogOpened}
+      />
     </div>
   );
 };
