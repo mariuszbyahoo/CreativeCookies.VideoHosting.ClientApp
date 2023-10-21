84821fd42b96b1883ab690ac9e8af6fd9b804e96
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-10-21 15:34:00 +0200

    implementing AboutComponent's contents from API

--- src/components/About/AboutComponent.js
+++ src/components/About/AboutComponent.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Card } from "reactstrap";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
 import { Button } from "@mui/material";
 import styles from "./AboutComponent.module.css";
 import { useAuth } from "../Account/AuthContext";
+import DOMPurify from "dompurify";
 
 const AboutComponent = () => {
   const { userRole, isAuthenticated } = useAuth();
   const navigate = useNavigate();
+  const [aboutContent, setAboutContent] = useState("");
+
+  useEffect(() => {
+    async function fetchAboutData() {
+      const apiResponse = await fetch(
+        `https://${process.env.REACT_APP_API_ADDRESS}/About`
+      );
+      const aboutResponseJson = await apiResponse.json();
+      const sanitizedHTML = DOMPurify.sanitize(aboutResponseJson.innerHTML); // Replace 'yourHtmlField' with your actual JSON field
+      setAboutContent(sanitizedHTML);
+    }
+    fetchAboutData();
+    debugger;
+  }, []);
+
   const editButton = () => {
     if (isAuthenticated && (userRole === "ADMIN" || userRole === "admin")) {
       return (
@@ -38,10 +54,21 @@ const AboutComponent = () => {
         {editButton()}
         <div>
           {overlay()}
-          <h1>Here's the homepage, a place to insert various stuff inside</h1>
-          <h4 style={{ textAlign: "center" }}>
-            Click <Link to="/films-list">Here</Link> to explore all films
-          </h4>
+          {aboutContent ? (
+            <div
+              style={{ margin: "1%" }}
+              dangerouslySetInnerHTML={{ __html: aboutContent }}
+            />
+          ) : (
+            <>
+              <h1>
+                Here's the homepage, a place to insert various stuff inside
+              </h1>
+              <h4 style={{ textAlign: "center" }}>
+                Click <Link to="/films-list">Here</Link> to explore all films
+              </h4>
+            </>
+          )}
         </div>
       </Card>
     </div>
