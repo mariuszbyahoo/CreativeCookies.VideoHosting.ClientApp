be04bd0959798c777f9390a4c26088cc65005f6b
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2023-11-11 10:00:38 +0100

    styling the SubscribeComponent correctly

--- src/components/Stripe/Subscribe/SubscribeComponent.js
+++ src/components/Stripe/Subscribe/SubscribeComponent.js
@@ -12,12 +12,14 @@ import {
   FormHelperText,
   MenuItem,
   Select,
+  TextField,
 } from "@mui/material";
 import { useAuth } from "../../Account/AuthContext";
 import styles from "./SubscribeComponent.module.css";
 import { ArrowForwardIos, HowToReg } from "@mui/icons-material";
 import ShopIcon from "@mui/icons-material/Shop";
 import { useEffect, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { t } from "i18next";
 
 const SubscribeComponent = () => {
@@ -33,6 +35,11 @@ const SubscribeComponent = () => {
   ] = useState(false);
   const [isOrderActiveDialogOpened, setIsOrderActiveDialogOpened] =
     useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const fetchPriceList = async () => {
     setLoading(true);
@@ -58,10 +65,161 @@ const SubscribeComponent = () => {
   }, [isAuthenticated]);
 
   const getActionButton = () => {
+    const colClassName = `${styles.addressContainer} col`;
+
     return isAuthenticated ? (
-      <Button variant="outlined" onClick={handleClick}>
-        <ShopIcon /> {t("Subscribe")}
-      </Button>
+      <div className={styles.container}>
+        <p style={{ fontWeight: 700 }}>Add invoice address:</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.container}>
+            <div className={styles.row}>
+              <div className={styles.field}>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  rules={{ required: "First name is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="FirstName"
+                      error={!!errors.firstName}
+                      helperText={
+                        errors.firstName ? errors.firstName.message : ""
+                      }
+                    />
+                  )}
+                />
+              </div>
+              <div className={styles.field}>
+                <Controller
+                  name="lastName"
+                  control={control}
+                  rules={{ required: "Last name is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="LastName"
+                      error={!!errors.lastName}
+                      helperText={
+                        errors.lastName ? errors.lastName.message : ""
+                      }
+                    />
+                  )}
+                />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.field}>
+                <Controller
+                  name="street"
+                  control={control}
+                  rules={{ required: "Street is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Street"
+                      error={!!errors.street}
+                      helperText={errors.street ? errors.street.message : ""}
+                    />
+                  )}
+                />
+              </div>
+              <div className={styles.field}>
+                <Controller
+                  name="houseNo"
+                  control={control}
+                  rules={{ required: "House number is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="House number"
+                      error={!!errors.houseNo}
+                      helperText={errors.houseNo ? errors.houseNo.message : ""}
+                    />
+                  )}
+                />
+              </div>
+              <div className={styles.field}>
+                <Controller
+                  name="appartmentNo"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Appartment number"
+                      error={!!errors.appartmentNo}
+                      helperText={
+                        errors.appartmentNo ? errors.appartmentNo.message : ""
+                      }
+                    />
+                  )}
+                />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.field}>
+                <Controller
+                  name="postCode"
+                  control={control}
+                  rules={{ required: "Post code is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Post code"
+                      error={!!errors.postCode}
+                      helperText={
+                        errors.postCode ? errors.postCode.message : ""
+                      }
+                    />
+                  )}
+                />
+              </div>
+              <div className={styles.field}>
+                <Controller
+                  name="city"
+                  control={control}
+                  rules={{ required: "City is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="City"
+                      error={!!errors.city}
+                      helperText={errors.city ? errors.city.message : ""}
+                    />
+                  )}
+                />
+              </div>
+              <div className={styles.field}>
+                <Controller
+                  name="Country"
+                  control={control}
+                  rules={{ required: "Country is required" }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      disabled="disabled"
+                      label="Country"
+                      error={!!errors.country}
+                      helperText={errors.country ? errors.country.message : ""}
+                      value="Polska"
+                    />
+                  )}
+                />
+              </div>
+            </div>
+          </div>
+          <p>
+            Above address will be used only for invoicing purposes and the tax
+            identification
+          </p>
+          <Button type="submit">
+            <ShopIcon /> {t("Subscribe")}
+          </Button>
+        </form>
+        {/* <Button variant="outlined" onClick={handleClick}>
+          <ShopIcon /> {t("Subscribe")}
+        </Button> */}
+      </div>
     ) : (
       <Button variant="outlined">
         <HowToReg /> {t("Register")}
@@ -72,16 +230,21 @@ const SubscribeComponent = () => {
   const handleDeclinedCoolingOffPeriodChange = (event) => {
     setHasDeclinedCoolingOffPeriod(event.target.checked);
   };
-
-  const handleClick = async () => {
-    openCheckoutSession();
-  };
-
-  const openCheckoutSession = async () => {
+  const onSubmit = async (addressData) => {
+    // Data contains your form fields
     const requestBody = {
+      address: addressData,
       priceId: selectedPriceId,
       hasDeclinedCoolingOffPeriod: hasDeclinedCoolingOffPeriod,
     };
+    // ... send data to your API
+    openCheckoutSession(requestBody);
+  };
+  // const handleClick = async () => {
+  //   openCheckoutSession();
+  // };
+
+  const openCheckoutSession = async (requestBody) => {
     const paymentSessionResult = await fetch(
       `https://${process.env.REACT_APP_API_ADDRESS}/StripeCheckout/CreateSession`,
       {
